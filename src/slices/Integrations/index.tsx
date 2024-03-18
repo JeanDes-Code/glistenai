c3db375bb11ce3923b46e20a27c03850d0116bd5
Jean Desauw <desauwjean@gmail.com>
2024-03-18 20:57:02 +0100

    add gsap animations

--- src/slices/Integrations/index.tsx
+++ src/slices/Integrations/index.tsx
@@ -5,31 +5,14 @@ import {
   SliceComponentProps,
 } from "@prismicio/react";
 import Image from "next/image";
-import {
-  FaDigitalOcean,
-  FaCloudflare,
-  FaNpm,
-  FaGithub,
-  FaFigma,
-  FaFly,
-} from "react-icons/fa6";
 
 import Bounded from "@/components/bounded";
 
 import StarBackground from "./star-background";
 import background from "./bg.jpg";
-import StylizedLogoMark from "./stylized-logo-mark";
 import React from "react";
-import clsx from "clsx";
 
-const icons = {
-  digitalocean: <FaDigitalOcean />,
-  cloudflare: <FaCloudflare />,
-  npm: <FaNpm />,
-  github: <FaGithub />,
-  figma: <FaFigma />,
-  fly: <FaFly />,
-};
+import { AnimatedContent } from "./animated-content";
 
 /**
  * Props for `Integrations`.
@@ -61,31 +44,7 @@ const Integrations = ({ slice }: IntegrationsProps): JSX.Element => {
         <div className="mx-auto mt-6 max-w-md text-balance text-center text-slate-300">
           <PrismicRichText field={slice.primary.body} />
         </div>
-        <div className="mt-20 flex flex-col items-center md:flex-row">
-          {slice.items.map((item, index) => (
-            <React.Fragment key={index}>
-              {index === Math.floor(slice.items.length / 2) && (
-                <>
-                  <StylizedLogoMark />
-                  <div className="signal-line rotate-180 bg-gradient-to-t" />
-                </>
-              )}
-              <div className="pulsing-icon flex aspect-square shrink-0 items-center justify-center rounded-full border border-blue-50/30 bg-blue-50/25 p-3 text-3xl text-blue-100 opacity-40 md:text-4xl lg:text-5xl">
-                {item.icon && icons[item.icon]}
-              </div>
-              {index !== slice.items.length - 1 && (
-                <div
-                  className={clsx(
-                    "signal-line",
-                    index >= Math.floor(slice.items.length / 2)
-                      ? "rotate-180"
-                      : "rotate-0",
-                  )}
-                />
-              )}
-            </React.Fragment>
-          ))}
-        </div>
+        <AnimatedContent slice={slice} />
       </div>
     </Bounded>
   );
